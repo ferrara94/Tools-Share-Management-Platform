bfe12362fcf3bd00b467aa277ec90b78f0cbc1a1
ferrara94 <ing.ferrara94@gmail.com>
2025-04-10 14:53:06 +0200

    Backend project added

---
+++ tool-sharing/src/main/java/com/ferrara/tool/ToolSharingApiApplication.java
@@ -0,0 +1,13 @@
+package com.ferrara.tool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToolSharingApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToolSharingApiApplication.class, args);
+	}
+
+}
---
+++ tool-sharing/src/test/java/com/ferrara/tool/ToolSharingApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ferrara.tool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToolSharingApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
