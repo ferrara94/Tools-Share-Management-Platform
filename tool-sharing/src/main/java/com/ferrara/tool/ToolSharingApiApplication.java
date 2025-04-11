45573a34413b602d0acb0a33e20d1ccf2a547671
ferrara94 <ing.ferrara94@gmail.com>
2025-04-11 15:29:26 +0200

    User, Role and Token classes added

--- tool-sharing/src/main/java/com/ferrara/tool/ToolSharingApiApplication.java
+++ tool-sharing/src/main/java/com/ferrara/tool/ToolSharingApiApplication.java
@@ -2,8 +2,10 @@ package com.ferrara.tool;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class ToolSharingApiApplication {
 
 	public static void main(String[] args) {
---
+++ tool-sharing/src/main/java/com/ferrara/tool/role/Role.java
@@ -0,0 +1,42 @@
+package com.ferrara.tool.role;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.ferrara.tool.user.User;
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@EntityListeners(AuditingEntityListener.class) //remember to have @EnableJpaAuditing in the main class
+public class Role {
+
+    @Id
+    @GeneratedValue
+    private Integer id;
+    @Column(unique = true)
+    private String name;
+
+    //many-to-many - mappedBy should be the role attribute in User
+    @ManyToMany(mappedBy = "roles")
+    @JsonIgnore //avoid the infinite loop
+    private List<User> users;
+
+    @CreatedDate
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    @Column(insertable = false)
+    private LocalDateTime lastModifiedDate;
+
+}
