9c8bfcd022b46eae8d31bced60f8c7df332a52e9
ferrara94 <ing.ferrara94@gmail.com>
2025-04-16 19:13:18 +0200

    OpenApi Documentation added

---
+++ tool-sharing/src/main/java/com/ferrara/tool/config/OpenApiConfig.java
@@ -0,0 +1,53 @@
+package com.ferrara.tool.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import io.swagger.v3.oas.annotations.servers.Server;
+
+@OpenAPIDefinition(
+        info = @Info(
+                contact = @Contact(
+                        name = "Felice",
+                        email = "contact@ferrara.com",
+                        url = "https://ferrara94.github.io/FerraraPortfolio.it"
+                ),
+                description = "OpenApi documentation for Spring Security",
+                title = "OpenApi specification",
+                version = "1.0",
+                license = @License(
+                        name = "License name",
+                        url = "https://license.com"
+                ),
+                termsOfService = "Terms of service"
+        ),
+        servers = {
+                @Server(
+                        description = "Local ENV",
+                        url = "http://localhost:8080/api/v1"
+                ),
+                @Server(
+                        description = "PROD ENV",
+                        url = "http://ferraratool.com/api/"
+                ),
+        },
+        security = {
+                @SecurityRequirement(
+                        name = "bearerAuth"
+                )
+        }
+)
+@SecurityScheme(
+        name = "bearerAuth",
+        description = "JWT auth description",
+        scheme = "bearer",
+        type = SecuritySchemeType.HTTP,
+        in = SecuritySchemeIn.HEADER
+)
+public class OpenApiConfig {
+}
