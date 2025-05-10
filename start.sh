21a7b1ee9ee9a2c7eb467e4d9d70c1ca674c9025
ferrara94 <ing.ferrara94@gmail.com>
2025-05-10 23:42:31 +0200

    start.sh refactor fix

--- start.sh
+++ start.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
+ROOT_DIR=$(pwd)
+
 echo "Building backend..."
-sudo docker build -t tsmp/tsmp:1.0.1 -f docker/backend/Dockerfile .
+sudo docker build -t tsmp/tsmp:1.0.1 -f $ROOT_DIR/docker/backend/Dockerfile $ROOT_DIR/tool-sharing
 
 echo "Building frontend..."
-sudo docker build -t tsmp/tsmp-ui:1.0.0 -f docker/frontend/Dockerfile .
+cd ../tool-sharing-ui
+
+sudo docker build -t tsmp/tsmp-ui:1.0.0 -f $ROOT_DIR/docker/frontend/Dockerfile $ROOT_DIR/tool-sharing-ui
 
 echo "Starting docker compose..."
 sudo docker compose up
