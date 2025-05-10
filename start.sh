f9bec9dc6fa0d0a86d6f7433f2cd689986c195f8
ferrara94 <ing.ferrara94@gmail.com>
2025-05-10 23:48:49 +0200

    typo removed

--- start.sh
+++ start.sh
@@ -6,7 +6,6 @@ echo "Building backend..."
 sudo docker build -t tsmp/tsmp:1.0.1 -f $ROOT_DIR/docker/backend/Dockerfile $ROOT_DIR/tool-sharing
 
 echo "Building frontend..."
-cd ../tool-sharing-ui
 
 sudo docker build -t tsmp/tsmp-ui:1.0.0 -f $ROOT_DIR/docker/frontend/Dockerfile $ROOT_DIR/tool-sharing-ui
 
